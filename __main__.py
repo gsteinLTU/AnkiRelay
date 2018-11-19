3c4dc3075d8fd28bd4e6694ffa5895af43993cbb
Gordon Stein <gstein@ltu.edu>
2018-11-19 16:01:15 -0600

    Robots list loaded

--- __main__.py
+++ __main__.py
@@ -9,12 +9,22 @@ env.read_env()
 server = env('SERVER')
 port = env('PORT')
 
+# Read robot list
+robots = []
+with open(".robots") as robotfile:
+    robots = robotfile.readlines()
+
+if robots == []:
+    raise Exception("No robots in .robots file.")
+
+print(robots)
+
 # Setup connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("127.0.0.1", 1974))
 
 while True:
 
-    
+
     data, addr = sock.recvfrom(1024)
     print("Received message " + str(data))
