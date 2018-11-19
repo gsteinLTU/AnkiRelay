eb066760b9115e101a704bb3f3e0595f59b7dd53
Gordon Stein <gstein@ltu.edu>
2018-11-19 17:08:16 -0600

    Say command working

--- __main__.py
+++ __main__.py
@@ -26,13 +26,14 @@ for robot in robots:
 # Setup server connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('localhost', 4791))
-sock.settimeout(1)
+sock.settimeout(2)
 
 while True:
     # Send announcement
     for robot in robots:
         msg = robot._name
-        sock.sendto(msg.encode("UTF-8"), (server, port))
+        print(msg.encode("ascii"))
+        sock.sendto(msg.encode("ascii"), (server, port))
         print('.')
     
     print('\n')
@@ -40,6 +41,21 @@ while True:
     # Wait for message
     try:
         data, addr = sock.recvfrom(128)
-        print("Received message " + str(data))
+        msg = data.decode('ascii')
+        print("Received message " + msg)
+
+        # Extract ID
+        msg = msg.split(':')
+
+        robot = [robot for robot in robots if robot._name == msg[0]]
+
+        if robot == []:
+            print("Invalid robot")
+        else:
+            msg = msg[1]
+
+            print("Received command " + msg)
+            if msg.startswith("say "):
+                robot[0].say_text(msg[4:])
     except:
         pass
