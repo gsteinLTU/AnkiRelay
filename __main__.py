dac789d36e388accc6aa2b6785f3032605f1a15a
Gordon Stein <gstein@ltu.edu>
2018-12-10 16:59:57 -0600

    Robots on own threads with own ports

--- __main__.py
+++ __main__.py
@@ -5,16 +5,78 @@ import re
 import string
 from environs import Env
 from commands import commands, requests
+import threading 
+import time
 
-def sendtoserver(msg):
-    print(msg)
-    sock.sendto(msg.encode("ascii"), (server, port))
+class RobotThread(threading.Thread):
+    lastport = 32323
+
+    def sendtoserver(self, msg):
+        print(msg)
+        self.sock.sendto(msg.encode("ascii"), (server, port))
+
+    def __init__(self, serial):
+        super().__init__()
+        self.serial = serial
+        
+    def run(self):
+        while True:
+            # Attempt to connect to robot
+            try:
+                self.robot = anki_vector.AsyncRobot(self.serial.strip())
+                self.robot.connect()
+
+                # Setup server connection
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                self.sock.bind(('localhost', RobotThread.lastport))
+                self.sock.settimeout(2)
+
+                RobotThread.lastport = RobotThread.lastport + 1
+                # Begin waiting for commands
+                while True:
+                    # Send announcement
+                    self.sendtoserver(self.serial)
+
+                    # Wait for message
+                    try:
+                        data, addr = self.sock.recvfrom(128)
+                        msg = data.decode('ascii')
+                        print("Received message " + msg)
+
+                        #     # Find command and run it
+                        #     for command in commands:
+                        #         if msg.startswith(command[0]):
+                        #             command[1](robot[0], msg)
+                        #             break
+
+                        #     if re.match("\\d+ .+", msg):
+                        #         seq = int(msg.split(' ')[0])
+                        #         msg = msg.lstrip(string.digits+' ')
+
+                        #         print("Sequence number " + str(seq))
+                        #         print("Received command " + msg)
+                        #         # Find requests and send result
+                        #         for request in requests:
+                        #             if msg.startswith(request[0]):
+                        #                 result = request[1](robot[0], msg)
+                        #                 sendtoserver(robot[0]._name + ": " + str(seq) + " " + result)
+                        #                 break
+                    
+                    except Exception as e:
+                        pass
+            except Exception as e:
+                pass
+
+            time.sleep(1)
+
+    
+
 
 # Read settings
 env = Env()
 env.read_env()
 server = env('SERVER')
-port = int(env('PORT'))
+port = 1973
 
 # Read robot list
 robots = []
@@ -25,65 +87,13 @@ if robots == []:
     raise Exception("No robots in .robots file.")
 
 # Connect to robots
-temp = [anki_vector.AsyncRobot(robot.strip()) for robot in robots]
-robots = []
+robots = [RobotThread(robot.strip()) for robot in robots]
 
-for robot in temp:
-    try:
-        robot.connect()
-        robots.append(robot)
-    except:
-        pass
-
-# Setup server connection
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('localhost', 4791))
-sock.settimeout(2)
+for robot in robots:
+    robot.start()
 
 while True:
-    # Send announcement
-    for robot in robots:
-        sendtoserver(robot._name)
-        print('.')
+    pass
     
-    print('\n')
 
-    # Wait for message
-    try:
-        data, addr = sock.recvfrom(128)
-        msg = data.decode('ascii')
-        print("Received message " + msg)
-
-        # Extract ID
-        msg = msg.split(':')
-
-        robot = [robot for robot in robots if robot._name == msg[0]]
-
-        # Verify robot 
-        if robot == []:
-            print("Invalid robot")
-        else:
-            msg = msg[1]
-
-            print("Received command " + msg)
-
-            # Find command and run it
-            for command in commands:
-                if msg.startswith(command[0]):
-                    command[1](robot[0], msg)
-                    break
-
-            if re.match("\\d+ .+", msg):
-                seq = int(msg.split(' ')[0])
-                msg = msg.lstrip(string.digits+' ')
-
-                print("Sequence number " + str(seq))
-                print("Received command " + msg)
-                # Find requests and send result
-                for request in requests:
-                    if msg.startswith(request[0]):
-                        result = request[1](robot[0], msg)
-                        sendtoserver(robot[0]._name + ": " + str(seq) + " " + result)
-                        break
-    except:
-        pass
+    
