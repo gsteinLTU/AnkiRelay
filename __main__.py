a9f336e6db3e5862ec43ea0ac54ca1286ac883bb
Gordon Stein <gstein@ltu.edu>
2019-01-18 16:35:41 -0600

    Was told LED is not necessary

--- __main__.py
+++ __main__.py
@@ -41,57 +41,12 @@ class RobotThread(threading.Thread):
         # Used to keep track of touches for hardware key trigger
         self.touch_count = 0
 
-        # # HW key size in numbers
-        # self.key_length = key_length
-        self.receiving_key = False
-        self.key_buff = []
-
-        # Status of virtual LEDs for display
-        self.leds = [0, 0]
-        self.ledtimer = threading.Timer(0.1, self.drawLEDs)
-
-    # Update value of virtual LED
-    def updateLED(self, pos, value):
-        # Validate length
-        if(pos > len(self.leds)):
-            return
-        
-        self.leds[pos] = value
-
-    # Display virtual LEDs on screen
-    def drawLEDs(self):
-        lastledstate = [0, 0]
-        
-        # Default to blank image
-        image = PIL.Image.new('RGBA', anki_vector.screen.dimensions(), (0,0,0,255))
-        image = anki_vector.screen.convert_image_to_screen_data(image)
-
-        while True:
-            # Don't redraw image if unneeded
-            if not all([lastledstate[i] == self.leds[i] for i in range(0,len(self.leds))]):
-                try:
-                    image = PIL.Image.new('RGBA', anki_vector.screen.dimensions(), (0,0,0,255))
-                    context = PIL.ImageDraw.Draw(image)
-
-                    for i in range(0,len(self.leds)):
-                        value = self.leds[i]
-                        context.ellipse([0 + 64 * i, 0, 32 + 64 * i, 32], fill=(value * 255,value * 255,value * 255,255))
-
-                    image = anki_vector.screen.convert_image_to_screen_data(image)
-                    lastledstate = [self.leds[i] for i in range(0,len(self.leds))]
-                except Exception as e:
-                    print(e)
-                              
-            self.robot.screen.set_screen_with_image_data(image, 0.2, interrupt_running=True)
-            time.sleep(0.2)
-
     def run(self):
         while True:
             # Attempt to connect to robot
             try:
                 self.robot = anki_vector.AsyncRobot(self.serial.strip(), enable_face_detection=False, enable_camera_feed=False)
                 self.robot.connect()
-                self.ledtimer.start()
                 self.robot.behavior.set_head_angle(anki_vector.util.degrees(45))
                 
                 # Setup server connection
@@ -126,15 +81,6 @@ class RobotThread(threading.Thread):
                             output += ord('R').to_bytes(1, 'big')
                             output += dist
                             self.send_info_to_server(output)
-                        elif command == 'L': #LED
-                            if self.receiving_key:
-                                self.key_buff.append(data)
-
-                                if len(self.key_buff) == 16:
-                                    self.receiving_key = False
-                                    print(self.key_buff)
-                            else:
-                                self.updateLED(data[1], data[2])
                         elif command == 'l': # Lift
                             h = struct.unpack('f', data[1:])[0]
                             print(h)
@@ -147,7 +93,6 @@ class RobotThread(threading.Thread):
             except Exception as e:
                 print(e)
             else:
-                self.ledtimer.cancel()
                 self.sock.close()
 
             time.sleep(1)
